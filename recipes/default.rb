fd63fbcc6d6522cc2fb057f6ab44289c44f16f69
Jim Rosser <jarosser06@gmail.com>
2015-05-06 11:19:52 -0500

    removed chef-client since it is out of scope of this cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -25,9 +25,6 @@ node.default['authorization']['sudo']['include_sudoers_d'] = true
   sudo
   rackspace_support::_rack
   chef-sugar-rackspace
-  chef-client::config
-  chef-client::delete_validation
-  chef-client
 ).each do |recipe|
   include_recipe recipe
 end
